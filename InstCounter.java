f2a54f5e40a36b5819976db7c1360f59cf0c41e1
s67122250096-pixel <s68122250008@ssru.ac.th>
2026-01-15 08:55:11 +0700

    Create InstCounter.java

---
+++ InstCounter.java
@@ -0,0 +1,19 @@
+
+public class InstCounter {
+
+    private static int counter; // ค่าเริ่มต้น 0
+
+    public InstCounter() {
+        ++counter;
+    }
+
+    public static int getCounter() {
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(InstCounter.getCounter()); // 0
+        InstCounter a1 = new InstCounter(); //มีการสร้าง obj = counter++
+        System.out.println(InstCounter.getCounter()); // 1
+    }
+}

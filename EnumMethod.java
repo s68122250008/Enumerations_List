aa9462caebb26926d24563ce901e1b638d104d4a
s67122250096-pixel <s68122250008@ssru.ac.th>
2026-01-22 08:47:41 +0700

    Update EnumMethod.java

--- EnumMethod.java
+++ EnumMethod.java
@@ -1,5 +1,4 @@
 
-
 public class EnumMethod {
 
     public static void main(String[] args) {
@@ -8,7 +7,7 @@ public class EnumMethod {
         for (Day day : Day.values()) {
             System.out.println("- " + day);
         }
-     
+
         Day friday = Day.FRIDAY;
         System.out.println("Name: " + friday.name()); // output: FRIDAY
         System.out.println("Ordinal of Friday: " + friday.ordinal()); // output: 4
